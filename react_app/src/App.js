b88b083a83c58ca4e4ef8d1272fc38856ba83517
wasborn200 <archibald.ff1@gmail.com>
2021-07-06 08:14:59 +0900

    react app page188 end

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,81 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
 
+let data = {title: 'Title',
+  message:'this is sample message.'};
+
+const SampleContext = React.createContext(data);
+
 class App extends Component {
-  data = [
-    "This is List Sample.",
-    "これはリストのサンプルです。",
-    "配列をリストに変換します。"
-  ];
-
-  msgStyle = {
-    fontSize:"20pt",
-    color:"#900",
-    margin:"20px 0px",
-    padding: "5px",
-  }
-
-  constructor(props){
-    super(props);
-    this.state = {
-      list: this.data
-    };
-  }
+  newdata = {title:'新しいタイトル',
+    message: 'これは新しいメッセージです'};
 
   render(){
-    return <div>
-      <h1>React</h1>
-      <h2 style={this.msgStyle}>show list.</h2>
-      <List title="サンプル・リスト" data={this.data} />
-    </div>
+    return (
+      <div>
+        <h1>Context</h1>
+        <Title />
+        <Message />
+        <SampleContext.Provider value={this.newdata}>
+          <Title />
+          <Message />
+        </SampleContext.Provider>
+        <Title />
+        <Message />
+      </div>
+    )
   }
 
 }
 
-class List extends Component {
-  number = 1;
-
-  title={
-    fontSize:"20pt",
-    fontWeight:"bold",
-    color:"blue",
-  }
+class Title extends Component {
+  static contextType = SampleContext;
 
   render(){
-    let data = this.props.data;
     return (
       <div>
-        <p style={this.title}>{this.props.title}</p>
-        <ul>
-          {data.map((item) =>
-            <Item number={this.number++} value={item}
-              key={this.number} />
-          )}
-        </ul>
+        <h2>{this.context.title}</h2>
       </div>
     )
   }
 }
 
-class Item extends Component{
-  li = {
-    listStyleType:"square",
-    fontSize:"16pt",
-    color:"#06",
-    margin:"0px",
-    padding: "0px",
-  }
-  num = {
-    fontWeight:"bold",
-    color:"red",
-  }
+class Message extends Component {
+  static contextType = SampleContext;
 
   render(){
-    return(
-      <li style={this.li}>
-        <span style={this.num}>[{this.props.number}] </span>
-          {this.props.value}
-      </li>
+    return (
+      <div>
+        <p>{this.context.message}</p>
+      </div>
     )
   }
 }
